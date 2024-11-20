9a47f5c72c9c2a442e5397fd26a6876f0c352aa2
Filipe Kevyn <FilipeKevyn4@gmail.com>
2024-11-20 16:19:10 -0300

    test: insert bet, contest and result to table

--- src/main/java/com/project/loteria/config/TestConfig.java
+++ src/main/java/com/project/loteria/config/TestConfig.java
@@ -1,22 +1,29 @@
 package com.project.loteria.config;
 
 import com.project.loteria.entities.Bet;
-import com.project.loteria.repositories.BetRepository;
+import com.project.loteria.entities.Contest;
+import com.project.loteria.service.BetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.util.Date;
+
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
     @Autowired
-    private BetRepository betRepository;
+    private BetService betService;
+
     @Override
     public void run(String... args) throws Exception {
         String[] jogo = new String[] {"1","2","4"};
         Bet bet1 = new Bet(null, "312", jogo);
 
-        betRepository.save(bet1);
+        betService.insert(bet1);
+
+        String[] drawNumber = new String[] {"1","2","3"};
+        Contest contest = new Contest(null, drawNumber, new Date());
     }
 }
