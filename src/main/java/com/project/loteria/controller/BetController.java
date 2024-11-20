bdd8df2d5d2defc0de7c53e9a0d182d4517de70c
Filipe Kevyn <FilipeKevyn4@gmail.com>
2024-11-19 22:13:52 -0300

    Implementation services

---
+++ src/main/java/com/project/loteria/controller/BetController.java
@@ -0,0 +1,26 @@
+package com.project.loteria.controller;
+
+import com.project.loteria.entities.Bet;
+import com.project.loteria.service.BetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/bet")
+public class BetController {
+    @Autowired
+    private BetService betService;
+
+    @GetMapping(value = "/{id}")
+    private ResponseEntity<Bet> findById(@PathVariable Long id){
+        Bet bet = betService.findById(id);
+        return ResponseEntity.ok().body(bet);
+    }
+
+    @PostMapping
+    public ResponseEntity<Bet> insert(@RequestBody Bet obj){
+        obj = betService.insert(obj);
+        return ResponseEntity.ok().body(obj); // trocar para um Response created
+    }
+}
---
+++ src/main/java/com/project/loteria/service/BetService.java
@@ -0,0 +1,23 @@
+package com.project.loteria.service;
+
+import com.project.loteria.entities.Bet;
+import com.project.loteria.repositories.BetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
+
+import java.util.Optional;
+
+@Service
+public class BetService {
+    @Autowired
+    private BetRepository betRepository;
+
+    public Bet findById(Long id){
+        Optional<Bet> bet = betRepository.findById(id);
+        return bet.orElseThrow(() -> new RuntimeException()); // criar excesão personalizada
+    }
+    public Bet insert(Bet obj){
+        return betRepository.save(obj);
+    }
+}
