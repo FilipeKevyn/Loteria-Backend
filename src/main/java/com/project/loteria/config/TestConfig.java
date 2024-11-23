32c676cbe0251461fee83ec794502e5c79c84563
Filipe Kevyn <FilipeKevyn4@gmail.com>
2024-11-22 21:48:55 -0300

    test: database insertions

--- src/main/java/com/project/loteria/config/TestConfig.java
+++ src/main/java/com/project/loteria/config/TestConfig.java
@@ -3,6 +3,7 @@ package com.project.loteria.config;
 import com.project.loteria.entities.Bet;
 import com.project.loteria.entities.Contest;
 import com.project.loteria.service.BetService;
+import com.project.loteria.service.ContestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +17,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private BetService betService;
 
+    @Autowired
+    private ContestService contestService;
+
     @Override
     public void run(String... args) throws Exception {
         String[] jogo = new String[] {"1","2","4"};
@@ -25,5 +29,7 @@ public class TestConfig implements CommandLineRunner {
 
         String[] drawNumber = new String[] {"1","2","3"};
         Contest contest = new Contest(null, drawNumber, new Date());
+
+        contestService.insert(contest);
     }
 }
