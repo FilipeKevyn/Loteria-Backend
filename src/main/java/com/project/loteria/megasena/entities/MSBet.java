ebcca0a6146fa5fa7a27c7d1ecd5efec0457cbcd
Filipe Kevyn <FilipeKevyn4@gmail.com>
2024-12-30 19:12:37 -0300

    Code bet unique

--- src/main/java/com/project/loteria/megasena/entities/MSBet.java
+++ src/main/java/com/project/loteria/megasena/entities/MSBet.java
@@ -16,7 +16,7 @@ public class MSBet implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String code;
     private double valueInvested;
     private int quantityNumbers;
