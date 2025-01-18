8844c79870acbd151e3839f14b25dfd4b194ea8c
Filipe Kevyn <FilipeKevyn4@gmail.com>
2025-01-18 14:21:15 -0300

    Add methods in interface

--- src/main/java/com/project/loteria/interfaces/BetService.java
+++ src/main/java/com/project/loteria/interfaces/BetService.java
@@ -2,10 +2,15 @@ package com.project.loteria.interfaces;
 
 
 import com.project.loteria.entities.Bet;
+import com.project.loteria.entities.Pool;
+
+import java.util.List;
 
 
 public interface BetService {
     Bet insert(Bet obj);
     void addBetToPool(Long poolId, Bet bet);
     void setMatched(Bet bet, int matched);
+    List<Integer> sortBet(List<Integer> bet);
+    boolean verifySameBet(Bet bet, Pool pool);
 }
