4a9a13f79abe2f1cf1d038bf9e0ec329eac574d4
Filipe Kevyn <FilipeKevyn4@gmail.com>
2025-02-04 19:04:54 -0300

    Delete interfaces

--- src/main/java/com/project/loteria/interfaces/ContestService.java
+++
@@ -1,8 +0,0 @@
-package com.project.loteria.interfaces;
-
-import com.project.loteria.entities.Contest;
-
-public interface ContestService {
-    Contest insert(Contest obj);
-    void setContestInPool(Long Id, Contest contest);
-}
