430562826fe03672b47f065133aa0a98817e3a1b
Filipe Kevyn <FilipeKevyn4@gmail.com>
2025-02-04 19:04:21 -0300

    Delete comments

--- src/main/java/com/project/loteria/config/TestConfig.java
+++ src/main/java/com/project/loteria/config/TestConfig.java
@@ -1,10 +1,5 @@
 package com.project.loteria.config;
 
-import com.project.loteria.repositories.BetRepository;
-import com.project.loteria.service.BetService;
-import com.project.loteria.service.ContestService;
-import com.project.loteria.service.PoolService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -12,25 +7,8 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
-    @Autowired
-    private ContestService contestService;
-
-    @Autowired
-    private BetService betService;
-
-    @Autowired
-    BetRepository betRepository;
-
-    @Autowired
-    private PoolService poolService;
     @Override
     public void run(String... args) throws Exception {
-//        MSPoolDTO dto = new MSPoolDTO("oi");
-//        poolService.createPool(dto);
 
-//        Integer[] jogo = new Integer[]{1,2,3,4,5,6,7,8,9,10};
-//        Bet bet1 = new Bet(null, "312", jogo, 10);
-//
-//        betService.addBetToPool(1L, bet1);
     }
 }
