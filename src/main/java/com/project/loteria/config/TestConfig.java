f53e9991f7d6c9610d0ebdecd98747bfd91c0606
Filipe Kevyn <FilipeKevyn4@gmail.com>
2025-04-14 17:57:30 -0300

    Profile test

---
+++ src/main/java/com/project/loteria/config/TestConfig.java
@@ -0,0 +1,47 @@
+package com.project.loteria.config;
+
+import com.project.loteria.entities.Bet;
+import com.project.loteria.entities.BetNumber;
+import com.project.loteria.entities.Pool;
+import com.project.loteria.repositories.PoolRepository;
+import com.project.loteria.service.BetService;
+import com.project.loteria.service.PoolService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+    @Autowired
+    private BetService service;
+
+    @Autowired
+    private PoolRepository poolRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        List<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+        Pool pool = new Pool();
+        pool.setTitle("Meu Bolão"); // Atribua algo ao título, pois ele pode ser obrigatório
+        pool.setType("Mega-Sena");
+
+        poolRepository.save(pool); // <-- Salve o pool primeiro
+
+        Bet bet = new Bet();
+        bet.setPool(pool);
+        bet.setBetNumbers(list);
+        bet.setQuantityNumbers(list.size());
+        bet.setType("Mega-Sena");
+
+        pool.getBets().add(bet); // opcional, mas ajuda a manter a consistência da entidade
+
+
+    }
+}
