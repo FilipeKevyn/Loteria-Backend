f4b0f6c0e70769ab723d1765b6b6df23eeb5ee5b
Filipe Kevyn <FilipeKevyn4@gmail.com>
2025-04-15 20:59:11 -0300

    Json Ignore in attribute BetNumber

--- src/main/java/com/project/loteria/entities/BetNumber.java
+++ src/main/java/com/project/loteria/entities/BetNumber.java
@@ -1,5 +1,6 @@
 package com.project.loteria.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.util.UUID;
@@ -15,6 +16,7 @@ public class BetNumber {
 
     private boolean matched = false;
 
+    @JsonIgnore
     @ManyToOne
     private Bet bet;
 
