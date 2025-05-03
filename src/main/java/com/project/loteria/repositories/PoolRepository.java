46cc355e3636a60309fdc6e8f31a1ead13922f0b
Filipe Kevyn <FilipeKevyn4@gmail.com>
2025-05-03 18:02:47 -0300

    Removed jpa repositories

--- src/main/java/com/project/loteria/repositories/PoolRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.loteria.repositories;
-
-import com.project.loteria.entities.Pool;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface PoolRepository extends JpaRepository<Pool, UUID> {
-}
--- src/main/java/com/project/loteria/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.loteria.repositories;
-
-import com.project.loteria.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface UserRepository extends JpaRepository<User, UUID> {
-}
