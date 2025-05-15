76d8f793ebe3237cf61a8e813745b75a5530d808
Filipe Kevyn <FilipeKevyn4@gmail.com>
2025-05-14 21:19:09 -0300

    test

--- src/main/java/com/project/loteria/config/TestConfig.java
+++ src/main/java/com/project/loteria/config/TestConfig.java
@@ -28,17 +28,15 @@ public class TestConfig implements CommandLineRunner {
         Pool pool = new Pool("Filipe");
         pool.setId(UUID.randomUUID());
         poolrepository.insert(pool);
+        System.out.println("Adicionou o pool");
 
         Bet bet = new Bet();
         bet.setId(UUID.randomUUID());
         bet.setValueInvested(100);
         bet.setPool(pool);
         betrepository.insert(bet);
+        System.out.println("Adicionou a bet ao pool");
 
-        pool.setValueTotal(100);
-        poolrepository.save(pool);
-
-        bet.setMatched(2);
-        betrepository.save(bet);
+        System.out.println(poolrepository.findById(pool.getId().toString()).getBets().toString());
     }
 }
