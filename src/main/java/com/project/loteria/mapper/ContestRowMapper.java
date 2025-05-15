3fbd5067eecefce13871a1162d3fad3a21308da6
FilipeKevyn <FilipeKevyn4@gmail.com>
2025-05-15 15:22:29 -0300

    remove imports

--- src/main/java/com/project/loteria/mapper/ContestRowMapper.java
+++ src/main/java/com/project/loteria/mapper/ContestRowMapper.java
@@ -1,15 +1,12 @@
 package com.project.loteria.mapper;
 
-import com.project.loteria.dao.repositories.PoolDAOImpl;
 import com.project.loteria.entities.Contest;
 import com.project.loteria.entities.Number;
 import com.project.loteria.entities.Pool;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
