8dbce2fc286183476614d2395d65666a48d58c60
Filipe Kevyn <FilipeKevyn4@gmail.com>
2025-05-14 21:16:40 -0300

    update tables

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,34 +5,35 @@ CREATE TABLE tb_pool (
     value_total_invested DECIMAL(10, 2)
 );
 
+CREATE TABLE tb_contest (
+    id UUID PRIMARY KEY,
+    pool_id UUID UNIQUE,
+    FOREIGN KEY (pool_id) REFERENCES tb_pool(id) ON DELETE CASCADE
+);
+
+CREATE TABLE tb_number (
+    id UUID PRIMARY KEY,
+    number INT,
+    matched BOOLEAN,
+    contest_id UUID,
+    pool_id UUID,
+    FOREIGN KEY (contest_id) REFERENCES tb_contest(id) ON DELETE CASCADE,
+    FOREIGN KEY (pool_id) REFERENCES tb_pool(id) ON DELETE CASCADE
+);
+
 CREATE TABLE tb_bet (
     id UUID PRIMARY KEY,
     value_invested DECIMAL(10, 2),
     game_type VARCHAR(50),
     matched INT,
     pool_id UUID,
-    FOREIGN KEY (pool_id) REFERENCES tb_pool(id)
+    FOREIGN KEY (pool_id) REFERENCES tb_pool(id) ON DELETE CASCADE
 );
 
-CREATE TABLE tb_bet_number (
-    id UUID PRIMARY KEY,
-    number INT,
-    matched BOOLEAN,
-    pool_id UUID,
-    FOREIGN KEY (pool_id) REFERENCES tb_pool(id)
-);
-
-CREATE TABLE tb_bet_numbers_bet (
+CREATE TABLE tb_numbers_bet (
     bet_id UUID,
-    bet_number_id UUID,
-    PRIMARY KEY (bet_id, bet_number_id),
-    FOREIGN KEY (bet_id) REFERENCES tb_bet(id),
-    FOREIGN KEY (bet_number_id) REFERENCES tb_bet_number(id)
-);
-
-CREATE TABLE tb_contest (
-    id UUID PRIMARY KEY,
-    draw_numbers TEXT,
-    pool_id UUID UNIQUE,
-    FOREIGN KEY (pool_id) REFERENCES tb_pool(id)
+    number_id UUID,
+    PRIMARY KEY (bet_id, number_id),
+    FOREIGN KEY (bet_id) REFERENCES tb_bet(id) ON DELETE CASCADE,
+    FOREIGN KEY (number_id) REFERENCES tb_number(id) ON DELETE CASCADE
 );
