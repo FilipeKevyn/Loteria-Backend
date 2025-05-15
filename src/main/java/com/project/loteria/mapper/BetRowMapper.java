89868cbb3d7090a9876d88157e48a0a801de0b37
Filipe Kevyn <FilipeKevyn4@gmail.com>
2025-05-14 21:18:06 -0300

    remove set on rowmapper

--- src/main/java/com/project/loteria/mapper/BetRowMapper.java
+++ src/main/java/com/project/loteria/mapper/BetRowMapper.java
@@ -1,16 +1,19 @@
 package com.project.loteria.mapper;
 
+import com.fasterxml.jackson.annotation.JacksonInject;
 import com.project.loteria.dao.repositories.PoolDAOImpl;
 import com.project.loteria.entities.Bet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.UUID;
 
+
 public class BetRowMapper implements RowMapper<Bet> {
-    @Autowired
+
     private PoolDAOImpl poolRepository;
 
     @Override
@@ -21,9 +24,6 @@ public class BetRowMapper implements RowMapper<Bet> {
         // setar betNumbers
         bet.setMatched(rs.getInt("matched"));
         bet.setType(rs.getString("game_type"));
-        // se lembrar de criar a FK pool_id para funcionar
-        bet.setPool(poolRepository.findById(rs.getString("pool_id")));
-
         return bet;
     }
 }
--- src/main/java/com/project/loteria/mapper/PoolRowMapper.java
+++ src/main/java/com/project/loteria/mapper/PoolRowMapper.java
@@ -1,5 +1,6 @@
 package com.project.loteria.mapper;
 
+import com.project.loteria.dao.PoolDAO;
 import com.project.loteria.dao.repositories.BetDAOImpl;
 import com.project.loteria.entities.Pool;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +12,6 @@ import java.sql.SQLException;
 import java.util.UUID;
 
 public class PoolRowMapper implements RowMapper<Pool> {
-    @Autowired
-    private BetDAOImpl betRepository;
 
     @Override
     public Pool mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -20,9 +19,8 @@ public class PoolRowMapper implements RowMapper<Pool> {
         pool.setId(UUID.fromString(rs.getString("id")));
         pool.setTitle(rs.getString("title"));
         pool.setType(rs.getString("type"));
-        pool.setValueTotal(rs.getDouble("value_total"));
+        pool.setValueTotal(rs.getDouble("value_total_invested"));
         // setar o contest
-        pool.setBets(betRepository.findBetsByPool(pool.getId().toString()));
         // setar users
         // setar betnumbers
 
