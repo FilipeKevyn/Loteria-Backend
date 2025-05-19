e92059475e0df7aa5bf08cc02cb6dc56de6c6cd8
José Thomaz <jose.thoomaz@gmail.com>
2025-05-19 06:46:15 -0300

    Create MinioClientConfig

---
+++ src/main/java/com/project/loteria/config/MinioClientConfig.java
@@ -0,0 +1,16 @@
+package com.project.loteria.config;
+
+import io.minio.MinioClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinioClientConfig {
+    @Bean
+    MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint("http://localhost:9000")
+                .credentials("MINIO_ACCESS_KEY","MINIO_SECRET_KEY")
+                .build();
+    }
+}
